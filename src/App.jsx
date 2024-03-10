d65a2d167130ae85bebb653acfcf64faba07b174
mvasim <mvasim>
2024-03-10 12:08:15 +0530

    update in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <>
+          <Navbar></Navbar>
           <h1 className="text-3xl font-bold underline">
               Hello world! {count} <button onClick={()=>{setCount(count+1)}}>+</button>
           </h1>
